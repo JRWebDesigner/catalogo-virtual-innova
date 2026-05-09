e29b022cc245816e667ee1bea77299b2c3f2fff0
TuUsuarioGitHub <tu_correo@ejemplo.com>
2026-05-09 08:17:24 -0400

    acu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
@@ -10,27 +9,23 @@ import Marcas from "./pages/Marcas.tsx";
 import MarcaDetalle from "./pages/MarcaDetalle.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <ProductsProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/productos" element={<Productos />} />
-            <Route path="/marcas" element={<Marcas />} />
-            <Route path="/marca/:brand" element={<MarcaDetalle />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </ProductsProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <ProductsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/marcas" element={<Marcas />} />
+          <Route path="/marca/:brand" element={<MarcaDetalle />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ProductsProvider>
+  </TooltipProvider>
 );
 
 export default App;
