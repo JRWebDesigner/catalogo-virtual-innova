4bc121e72634ace7324bd56a13adde2a2a28246d
josue mamani <jowebdesigc@gmail.com>
2026-06-12 15:29:35 -0400

    code number

--- src/components/store/ProductCard.tsx
+++ src/components/store/ProductCard.tsx
@@ -47,10 +47,15 @@ export const ProductCard = ({ product, onEdit, onDelete }: ProductCardProps) => 
             </div>
           )}
         </div>
-
-        <div className="absolute left-4 top-4 rounded-full border border-white/15 bg-slate-950/70 px-3 py-1 text-xs font-semibold uppercase tracking-[0.24em] text-white/80 shadow-sm">
-          {product.code}
-        </div>
+        {
+          product.code === "0" ? (<>
+           </>) : (<> 
+            <div className="absolute left-4 top-4 rounded-full border border-white/15 bg-slate-950/70 px-3 py-1 text-xs font-semibold uppercase tracking-[0.24em] text-white/80 shadow-sm">
+             {product.code}
+            </div> 
+          </>)
+        }
+        
         <div className="absolute right-4 bottom-4 rounded-2xl bg-white/10 px-3 py-2 text-[11px] font-semibold uppercase tracking-[0.2em] text-white/80 backdrop-blur-sm">
           {product.capacity}
         </div>
